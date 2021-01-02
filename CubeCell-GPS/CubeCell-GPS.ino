d36260035f2bd0989f62d25a4ea94b18d4adaf2e
David Brodrick <brodrick@gmail.com>
2021-01-02 17:05:07 +1100

    Display when no GPS and ensure don't send old GPS data from serial buffer.

--- CubeCell-GPS/CubeCell-GPS.ino
+++ CubeCell-GPS/CubeCell-GPS.ino
@@ -78,52 +78,57 @@ void setup() {
   LoRaWAN.displayMcuInit();
   deviceState = DEVICE_STATE_INIT;
   LoRaWAN.ifskipjoin();
-
-  //Initialise GPS
-  Air530.begin();    
 }
 
 void loop()
 {
-  if (joined) {    
-    char str[30];
-    display.clear();
-    display.setFont(ArialMT_Plain_10);
-    int index = sprintf(str,"%02d-%02d-%02d",Air530.date.year(),Air530.date.day(),Air530.date.month());
-    str[index] = 0;
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.drawString(0, 0, str);
+  if (joined) {
+    if (Air530.date.isValid() && Air530.location.age() < 10000) {    
+      char str[30];
+      display.clear();
+      display.setFont(ArialMT_Plain_10);
+      int index = sprintf(str,"%02d-%02d-%02d",Air530.date.year(),Air530.date.day(),Air530.date.month());
+      str[index] = 0;
+      display.setTextAlignment(TEXT_ALIGN_LEFT);
+      display.drawString(0, 0, str);
   
-    index = sprintf(str,"%02d:%02d:%02d",Air530.time.hour(),Air530.time.minute(),Air530.time.second(),Air530.time.centisecond());
-    str[index] = 0;
-    display.drawString(60, 0, str);
+      index = sprintf(str,"%02d:%02d:%02d",Air530.time.hour(),Air530.time.minute(),Air530.time.second(),Air530.time.centisecond());
+      str[index] = 0;
+      display.drawString(60, 0, str);
 
-    if( Air530.location.age() < 5000 )  {
-      display.drawString(120, 0, "A");
-    } else {
-      display.drawString(120, 0, "V");
-    }
+      if( Air530.location.age() < 10000 )  {
+        display.drawString(120, 0, "A");
+      } else {
+        display.drawString(120, 0, "V");
+      }
   
-    index = sprintf(str,"alt: %d.%d",(int)Air530.altitude.meters(),fracPart(Air530.altitude.meters(),2));
-    str[index] = 0;
-    display.drawString(0, 16, str);
+      index = sprintf(str,"alt: %d.%d",(int)Air530.altitude.meters(),fracPart(Air530.altitude.meters(),2));
+      str[index] = 0;
+      display.drawString(0, 16, str);
    
-    index = sprintf(str,"hdop: %d.%d",(int)Air530.hdop.hdop(),fracPart(Air530.hdop.hdop(),2));
-    str[index] = 0;
-    display.drawString(0, 32, str); 
+      index = sprintf(str,"hdop: %d.%d",(int)Air530.hdop.hdop(),fracPart(Air530.hdop.hdop(),2));
+      str[index] = 0;
+      display.drawString(0, 32, str); 
  
-    index = sprintf(str,"lat :  %d.%d",(int)Air530.location.lat(),fracPart(Air530.location.lat(),4));
-    str[index] = 0;
-    display.drawString(60, 16, str);   
+      index = sprintf(str,"lat :  %d.%d",(int)Air530.location.lat(),fracPart(Air530.location.lat(),4));
+      str[index] = 0;
+      display.drawString(60, 16, str);   
   
-    index = sprintf(str,"lon:%d.%d",(int)Air530.location.lng(),fracPart(Air530.location.lng(),4));
-    str[index] = 0;
-    display.drawString(60, 32, str);
+      index = sprintf(str,"lon:%d.%d",(int)Air530.location.lng(),fracPart(Air530.location.lng(),4));
+      str[index] = 0;
+      display.drawString(60, 32, str);
 
-    index = sprintf(str,"speed: %d.%d km/h",(int)Air530.speed.kmph(),fracPart(Air530.speed.kmph(),3));
-    str[index] = 0;
-    display.drawString(0, 48, str);
-    display.display();
+      index = sprintf(str,"speed: %d.%d km/h",(int)Air530.speed.kmph(),fracPart(Air530.speed.kmph(),3));
+      str[index] = 0;
+      display.drawString(0, 48, str);
+      display.display();
+    } else {
+      display.clear();
+      display.setFont(ArialMT_Plain_16);
+      display.setTextAlignment(TEXT_ALIGN_CENTER);
+      display.drawString(64, 20, "NO GPS LOCK");
+      display.display();      
+    }
    
     uint32_t starttime = millis();
     while( (millis()-starttime) < 1000 ) {
@@ -146,8 +151,15 @@ void loop()
       break;
     }
     case DEVICE_STATE_SEND: {
+      if (!joined) {
+        //We have just joined. Purge any old characters from GPS serial stream
+        Air530.begin();    
+        while (Air530.available()) {
+          Air530.read();
+        }
+      }
       joined=true;
-      if( Air530.location.isValid() && Air530.altitude.isValid() && Air530.location.age() < 5000) {      
+      if( Air530.location.isValid() && Air530.altitude.isValid() && Air530.location.age() < 10000) {      
         prepareTxFrame( appPort );
         LoRaWAN.send();      
       }
